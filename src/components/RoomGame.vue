60bf82a3e45ea5c2bd86b1778b6cef56ee6c6361
Isnan Irsyad <sao.irsyad@gmail.com>
2019-06-28 14:00:10 +0700

    add more feature to game

--- src/components/RoomGame.vue
+++ src/components/RoomGame.vue
@@ -1,80 +1,170 @@
 <template>
-  <div class>
-    <h1>{{ players_in_room[0].room_name }}</h1>
-    
-    <hr>
-    <h3>OR</h3>
-    <hr>
-    <H1>GAME START</H1>
+  <div>
     <audio id="correct-sound">
       <source src="../../public/Quiz-correct-sound-with-applause.mp3" type="audio/mpeg">
     </audio>
     <audio id="false-sound">
       <source src="../../public/Family-feud-buzzer.wav" type="audio/mpeg">
     </audio>
-    <div class="row">
+    <div v-if="playing == false" class="center">
+      <h1>{{ status }}</h1>
+      <div class="col s12 m6">
+        <div :class="['card', isFull ? 'blue' : 'grey']">
+          <div class="card-content white-text">
+            <span class="card-title">{{ players_in_room[0].room_name }}</span>
+            <div class="collection">
+              <a
+                href="#"
+                v-for="player in players_in_room[0].players"
+                :key="player"
+                :class="['collection-item',player.name == whoAmI ? 'active' : '']"
+              >{{ player.name }}</a>
+            </div>
+          </div>
+          <div class="row">
+            <a @click="leave_room" class="btn btn-large red left" style="margin-left: 100px;">Leave Room</a>
+            <a
+              v-if="isFull && players_in_room[0].owner == whoAmI"
+              @click="start_game"
+              class="btn btn-large cyan pulse right"
+              style="margin-right: 100px;"
+            >Start Game</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-if="playing == true" class="row">
+      <!-- <a @click="start_game" class="btn btn-large cyan pulse">Start Game</a>
+      <h1 v-show="!started">Ready?</h1>
+      <h1>{{ countDown }}</h1> -->
       <div class="col s12 m7 l7">
         <div class="card">
+          <h1>{{ index + 1 }} / 10 </h1>
           <div class>
-            <img :src="this.$store.state.quizzes[this.index].question" alt class="img-top">
+            <img :src="$store.state.quizzes[index].question" alt class="img-top">
           </div>
-          <div class>
-            <input type="text" v-model="answerText" @keyup.enter="answer()">
-            <a href="#" class="btn btn-sm ml-2 btn-primary" @click="answer()">Send</a>
+          <div class="input-field">
+            <input
+            :class="['validate', isCorrect == false ? 'invalid' : '']"
+              placeholder="Your Answer"
+              type="text"
+              v-model="answerText"
+              @keyup.enter="answer()"
+              autofocus
+            >
+            <span class="helper-text" data-error="Wrong Answer, try again"></span>
+            <!-- <a href="#" class="btn btn-sm ml-2 btn-primary" @click="answer()">Send</a> -->
           </div>
         </div>
       </div>
       <div class="col s12 m5 l5">
         <div class="card">
           <h1>Score</h1>
-          <hr>
-          <div class="card-body" v-for="(player, index) in players_in_room[0].players" :key="index">
-            <div class="left">
-              <span>PLAYER NAME : {{ player.name }}</span>
-            </div><br>
-            <div class="left">
-              <span>SCORE : {{ player.score }}</span>
-            </div>
-            <div class="progress">
-              <div class="determinate" style="width: 90%"></div>
+          <div class="card-body">
+            <div
+              class="collection"
+            >
+              <a href="#" :class="['collection-item',player.name == whoAmI ? 'active blue lighten-2' : '']" v-for="(player, index) in players_in_room[0].players"
+              :key="index"
+              style="margin: 0;">
+                <div class="left">
+                  <span>{{ player.name }}</span>
+                </div>
+                <br>
+                <div class="left">
+                  <span>{{ player.score }}</span>
+                </div>
+                <div class="progress">
+                  <div class="determinate" :style="{'width': progressBar(player.score)}"></div>
+                </div>
+              </a>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="center">
-      <button @click="leave_room" class="btn danger">LEAVE ROOM</button>
     </div>
   </div>
 </template>
 
 <script>
 import db from "../config/firebase";
+import { setInterval, clearInterval, setTimeout } from 'timers';
 
 export default {
+
   data() {
     return {
       index: 0,
       answerText: "",
-      players_in_room: []
+      players_in_room: [],
+      playing: true,
+      isCorrect: null,
+      timer: 30,
+      count: 3,
+      started: false
     };
   },
+  computed: {
+    countDown(){
+      if(this.started)
+        return this.timer
+      else
+        return this.count
+    },
+    whoAmI() {
+      return localStorage.getItem("username_tebakgambar");
+    },
+    isFull() {
+      return (
+        this.players_in_room[0] && this.players_in_room[0].players.length >= 2
+      );
+    },
+    status() {
+      if (this.isFull) {
+        return "Ready";
+      } else return "Waiting";
+    },
+  },
   methods: {
+    progressBar(score){
+      return score + '%'
+    },
+    start_game(){
+      this.playing = true
+      // var count = setInterval(()=>{
+      //     this.count--
+      //   },1000)
+      // setTimeout(()=>{
+      //   this.started = true
+      //   this.count = 3
+      //   clearInterval(count)
+      //   var timer = setInterval(()=>{
+      //     this.timer--
+      //   },1000)
+      //   setTimeout(()=>{ 
+      //     console.log('=====================================');
+      //     this.timer = 30
+      //     this.started = false
+      //     clearInterval(timer)
+      //    }, Number(this.timer+'000'));
+
+      // },3000)
+    },
     fetchPlayer() {
       db.collection("rooms").onSnapshot(querySnapshot => {
-      let players_temp = [];
-      let roomID = localStorage.getItem("roomId_tebakgambar");
-      querySnapshot.forEach(doc => {
-        if (doc.id === roomID) {
-          players_temp.push({
-            id: doc.id,
-            ...doc.data()
-          });
-        }
+        let players_temp = [];
+        let roomID = localStorage.getItem("roomId_tebakgambar");
+        querySnapshot.forEach(doc => {
+          if (doc.id === roomID) {
+            players_temp.push({
+              id: doc.id,
+              ...doc.data()
+            });
+          }
+        });
+        this.players_in_room = players_temp;
+        console.log("Created invoke ");
       });
-      this.players_in_room = players_temp;
-      console.log("Created invoke ");
-    });
     },
     leave_room() {
       db.collection("rooms")
@@ -117,20 +207,25 @@ export default {
           let players = doc.data().players
           let idx = doc.data().players.findIndex(i => i.name === localStorage.getItem('username_tebakgambar'))
           players[idx].score += 10;
-          
+
           return db.collection("rooms")
           .doc(localStorage.getItem('roomId_tebakgambar'))
           .update({ players: players })
         })
         .then(() => {
             console.log('Success update score')
+            if (this.$store.state.quizzes.length - 1 === this.index) {
+              
+            }
             setTimeout(() => {
+              this.isCorrect = null
               this.index++;
             }, 5000)
           })
         .catch(err => console.log(err))
       }
       else {
+        this.isCorrect = false
         const sound = document.getElementById('false-sound')
         sound.play()
         this.answerText = "";
@@ -138,7 +233,7 @@ export default {
     }
   },
   created() {
-    this.fetchPlayer()
+    this.fetchPlayer();
     if (localStorage.getItem("roomId_tebakgambar")) {
       this.$router.push(
         `/rooms/games/${localStorage.getItem("roomId_tebakgambar")}`
@@ -146,17 +241,6 @@ export default {
     } else {
       this.$router.push("/rooms");
     }
-
-    // db.collection("rooms")
-    //   .doc(localStorage.getItem('roomId_tebakgambar'))
-    //   .get()
-    //   .then(doc => {
-    //     console.log('tes player', doc.data().players)
-    //     let idx = doc.data().players.findIndex(i => i.name === localStorage.getItem('username_tebakgambar'))
-    //     console.log('you player', doc.data().players[idx])
-        
-    //   })
-
   }
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-Vue.config.devtools = true
 
 export default new Vuex.Store({
   state: {
